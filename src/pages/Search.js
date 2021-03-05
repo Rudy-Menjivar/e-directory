3c60b5a0ca96c6e91d8d9cc7b6ba6048f647ba28
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-05 00:57:09 -0800

    created Seach component class with export

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,2 +1,12 @@
 import React, { Component } from "react";
 import API from "../utils/API";
+
+class Search extends Component {
+    state = {
+      search: "",
+      results: [],
+      error: ""
+    };
+}
+
+export default Search;
