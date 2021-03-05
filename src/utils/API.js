d389bfcf2e3b412ffdc12ad30b88725da5fb235a
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-05 00:35:34 -0800

    import axios to fetch up to 25 U.S. users with randomusers api

---
+++ src/utils/API.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import axios from "axios";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    search: function() {
+        return axios.get("https://randomuser.me/api/1.3/?results=25&nat=us");
+    }
+};
