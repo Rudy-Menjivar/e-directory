eb012c5495a86353b0e63c75761b3e4abbd0444d
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-09 14:46:20 -0800

    API added to return 25 U.S. employee list

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    search: function() {
+    search25: function() {
         return axios.get("https://randomuser.me/api/1.3/?results=25&nat=us");
     }
 };
