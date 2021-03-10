79b07b9e4d3e83cc77c2c91126d3d91905e2eca1
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-09 18:34:55 -0800

    Return RenderData

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import RenderData from "../RenderData";
 
 export default class Main extends Component {
   render() {
     return (
       <>
+        <RenderData />
       </>
     );
   }
