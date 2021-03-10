8a106b0f3c6b98eb527e5955ce59278ed73835d4
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-09 18:34:07 -0800

    Set heading name values along with widths. Also return data from Navbar and RenderTable

---
+++ src/components/RenderData/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+import Navbar from "../Navbar";
+import RenderTable from "../RenderTable";
+import "./style.css";
+
+export default class RenderData extends Component {
+  state = {
+    users: [{}],
+    order: "descend",
+    filteredUsers: [{}]
+  }
+
+  headings = [
+    { name: "Image", width: "15%" },
+    { name: "Name", width: "25%" },
+    { name: "Phone", width: "20%" },
+    { name: "Email", width: "25%" },
+    { name: "DOB", width: "15%" }
+  ]
+
+  render() {
+    return (
+      <>
+        <Navbar handleInputData={this.handleInputData} />
+        <div className="render-data">
+          <RenderTable
+            headings={this.headings}
+            users={this.state.filteredUsers}
+            handleSort={this.handleSort}
+          />
+        </div>
+      </>
+    );
+  }
+}
