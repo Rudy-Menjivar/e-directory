88b54b3385daf599c3a5611e4d6ad6fe60fb4889
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-09 18:53:23 -0800

    import RenderBody to populate users

--- src/components/RenderTable/index.js
+++ src/components/RenderTable/index.js
@@ -1,6 +1,7 @@
 import React from "react";
+import RenderBody from "../RenderBody";
 
-function RenderTable({ headings }) {
+function RenderTable({ headings, users }) {
   return (
     <div className="render-table mt-5">
       <table
@@ -19,6 +20,8 @@ function RenderTable({ headings }) {
             })}
           </tr>
         </thead>
+
+        <RenderBody users={users} />
       </table>
     </div>
   );
